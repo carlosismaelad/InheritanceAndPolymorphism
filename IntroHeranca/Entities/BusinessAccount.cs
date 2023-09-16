2ae841df8c705216f560a0a9b6f06b90e1b676fe
carlosismaelad <carlosismaelad@outlook.com>
2023-09-16 16:02:58 -0300

    Herança e modificador de acesso protected

---
+++ IntroHeranca/Entities/BusinessAccount.cs
@@ -0,0 +1,29 @@
+namespace IntroHeranca.Entities;
+
+public class BusinessAccount : Account  
+{
+    public double LoanLimit { get; set; }
+
+
+    public BusinessAccount()
+    {
+    }
+
+    public BusinessAccount(double loanLimit)
+    {
+        LoanLimit = loanLimit;
+    }
+
+    public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+    {
+        LoanLimit = loanLimit;
+    }
+
+    public void Loan(double amount)
+    {
+        if (amount <= LoanLimit)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ IntroHeranca/Program.cs
@@ -0,0 +1,19 @@
+﻿using IntroHeranca.Entities;
+
+namespace IntroHeranca
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            BusinessAccount businessAccount = new BusinessAccount(8010, "Carlos", 400.00, 500.00);
+            
+            Console.WriteLine(businessAccount.Balance);
+            
+            // Não será permitido:
+            // businessAccount.Balance = 800.00;
+        }
+    }
+}
