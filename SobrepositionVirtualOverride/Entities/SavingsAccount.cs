bb10b5184a5424b8ad1163dc609fb5decadbc073
carlosismaelad <carlosismaelad@outlook.com>
2023-09-18 11:08:57 -0300

    Virtual, Override e Base - Sobreposição de métodos

---
+++ SobrepositionVirtualOverride/Entities/SavingsAccount.cs
@@ -0,0 +1,28 @@
+namespace SobrepositionVirtualOverride.Entities;
+
+
+public class SavingsAccount : Account
+{
+    public double InterestRate { get; set; }
+
+    public SavingsAccount()
+    {
+        
+    }
+
+    public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+    {
+        InterestRate = interestRate;
+    }
+
+    public void UpdateBalance()
+    {
+        Balance += Balance * InterestRate;
+    }
+
+    public override void Withdraw(double amount)
+    {
+        base.Withdraw(amount);
+        Balance -= 3.0;
+    }
+}
---
+++ SobrepositionVirtualOverride/Program.cs
@@ -0,0 +1,26 @@
+﻿using SobrepositionVirtualOverride.Entities;
+
+namespace SobrepositionVirtualOverride
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Account acc1 = new Account(1001, "Alex", 500.00);
+            Account acc2 = new SavingsAccount(1002, "Ana", 500.00, 0.01);
+            
+            // Testando nossa regra de saques:
+            acc1.Withdraw(10.00);
+            acc2.Withdraw(10.00);
+
+            Console.WriteLine(acc1.Balance); // Descontou 10,00 + 2,00 de taxa
+            Console.WriteLine(acc2.Balance); // Descontou 10,00 + 5,00 de taxa
+        }
+
+
+
+        
+    }
+}
